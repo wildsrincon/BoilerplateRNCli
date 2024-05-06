c14693b1f5bb2f97251125007a197ad61889065b
Wilder Rincon <wildsrincon.developer@gmail.com>
2024-05-05 22:04:38 -0500

    implement husky and lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,8 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    "plugin:jest/recommended",
-    'plugin:testing-library/react'
+    'plugin:jest/recommended',
+    'plugin:testing-library/react',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
@@ -23,18 +23,12 @@ module.exports = {
       jsx: true,
     },
   },
-  plugins: [
-    'react',
-    'react-native',
-    'react-hooks',
-    'unused-imports',
-    '@typescript-eslint',
-  ],
+  plugins: ['react', 'react-native', 'react-hooks', 'unused-imports', '@typescript-eslint'],
   overrides: [
     {
       // Test files only
       files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-    }
+    },
   ],
   rules: {
     'prettier/prettier': 2,
--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const a = 12;
-
 export default function App() {
   return (
     <View style={styles.container}>
