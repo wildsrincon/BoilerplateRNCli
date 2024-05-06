2acba184a7ab9334156f299685f1d71f523c32b2
Wilder Rincon <wildsrincon.developer@gmail.com>
2024-05-05 22:01:22 -0500

    test husky hook

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const a = 12;
+
 export default function App() {
   return (
     <View style={styles.container}>
